ced444b56bac96da982a6ce4f49e7336e7f7f6db
Sergey Kolesnikov <trm@mail.ru>
2025-11-03 06:43:21 +0300

    7 - some fixes, some refac

--- src/layouts/DefaultLayout/DefaultLayout.vue
+++ src/layouts/DefaultLayout/DefaultLayout.vue
@@ -23,11 +23,12 @@ import DefaultLayoutSideNavigation from './components/DefaultLayoutSideNavigatio
 
 <style lang="css" scoped>
 .default-layout {
-  width: 100%;
-  height: 100%;
+  max-width: var(--app-app-max-width);
+  width: var(--app-app-width);
+  height: var(--app-app-height);
+  margin: 0;
   /* leave space for header and sidebar */
   padding: var(--app-header-height) 0 0 var(--app-sidebar-width);
-  margin: 0;
   box-sizing: border-box;
   background-color: var(--app-body-bg);
   color: var(--app-text-primary);
@@ -37,7 +38,6 @@ import DefaultLayoutSideNavigation from './components/DefaultLayoutSideNavigatio
   position: absolute;
   left: 0;
   top: 0;
-  /* bottom: 0; */
   right: 0;
   height: var(--app-header-height);
   background-color: var(--app-panel-bg);
@@ -50,7 +50,6 @@ import DefaultLayoutSideNavigation from './components/DefaultLayoutSideNavigatio
   left: 0;
   top: var(--app-header-height);
   bottom: 0;
-  /* right: 0; */
   width: var(--app-sidebar-width);
   background-color: var(--app-sidebar-bg);
   border-right: 1px solid var(--app-sidebar-border);
